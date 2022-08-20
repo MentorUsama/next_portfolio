394fd7c6720bd391b671503c6426797db032fe57
unknown <muhammadusamafarhat100@gmail.com>
2022-08-20 16:06:37 +0500

    max width added

--- wrappers/Wrapper.js
+++ wrappers/Wrapper.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {useTheme} from '../context'
 import {Footer,Navigation} from '../components/index'
+import * as classes from './Wrapper.module.scss'
 
 export default function Wrapper(props) {
   const theme=useTheme()
   return (
-    <div className={theme.mode}>
+    <div className={`${theme.mode=="light"?classes.light:classes.dark} ${classes.wrapper}`}>
+        <div>
         <Navigation mode={theme.mode} setMode={theme.setMode} />
         {props.children}
         <Footer mode={theme.mode} />
+        </div>
     </div>
   )
 }
